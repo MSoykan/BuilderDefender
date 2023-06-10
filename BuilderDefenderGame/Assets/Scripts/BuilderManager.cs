55c3001eb9bce8dcc39955bbdf91ce41c603a3b5
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-06-10 15:06:28 +0300

    Added ResourceTypes SO and ResourceManager script
    Every resource type is a scriptable object,
    Resource Manager is in charge of loading resourceTypes from "Resources" folder

--- BuilderDefenderGame/Assets/Scripts/BuilderManager.cs
+++ BuilderDefenderGame/Assets/Scripts/BuilderManager.cs
@@ -8,12 +8,15 @@ public class BuilderManager : MonoBehaviour {
     private BuildingTypeListSO buildingTypeList;
     private BuildingTypeSO buildingType;
 
+    private void Awake() {
+        buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+        Debug.Log("Buildings: " + Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name));
+        buildingType = buildingTypeList.list[0];
+    }
 
     private void Start() {
         mainCamera = Camera.main;
 
-        buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
-        buildingType = buildingTypeList.list[0];
     }
 
     private void Update() {
