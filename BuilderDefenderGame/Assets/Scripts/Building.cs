bf10397e53fffd725d98402019f24bcd8454dcb0
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-06-14 16:31:08 +0300

    Added DemolishBuilding feature
    Demolish button is shown upon hovering over buildings, and destroys the gameobject when its clicked.

--- BuilderDefenderGame/Assets/Scripts/Building.cs
+++ BuilderDefenderGame/Assets/Scripts/Building.cs
@@ -6,26 +6,48 @@ public class Building : MonoBehaviour
 {
     private BuildingTypeSO buildingType;
     private HealthSystem healthSystem;
+    private Transform buildingDemolishBtn;
+
+    private void Awake() {
+        buildingDemolishBtn = transform.Find("pfBuildingDemolishBtn");
+        if(buildingDemolishBtn != null) {
+        buildingDemolishBtn.gameObject.SetActive(false);
+        }
+    }
 
     private void Start() {
         buildingType = GetComponent<BuildingTypeHolder>().buildingType;
         healthSystem = GetComponent<HealthSystem>();
 
-
         healthSystem.SetHealthAmountMax(buildingType.healthAmountMax, true);
 
         healthSystem.OnDied += HealthSystem_OnDied;
-
     }
 
     public void Update() {
-        //if(Input.GetKeyDown(KeyCode.T)) {
-        //    healthSystem.Damage(30);
-        //    Debug.Log(healthSystem.GetHealthAmount());  
-        //}
+
     }
 
     private void HealthSystem_OnDied(object sender, System.EventArgs e) {
         Destroy(gameObject);
+    }
+
+    private void OnMouseEnter() {
+        ShowBuildingDemolishButton();
+    }
+    private void OnMouseExit() {
+        HideBuildingDemolishButton();
+    }
+
+    private void ShowBuildingDemolishButton() {
+        if (buildingDemolishBtn != null) {
+            buildingDemolishBtn.gameObject.SetActive(true);
+        }
+    }
+
+    private void HideBuildingDemolishButton() {
+        if (buildingDemolishBtn != null) {
+            buildingDemolishBtn.gameObject.SetActive(false);
+        }
     }
 }
